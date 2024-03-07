7b8987a0c2172cfb7b6b8ddf8d53d5cff996d8f0
balqisnaguwib <balqisnaguwib@gmail.com>
2024-03-07 10:32:10 +0800

    change position

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -60,7 +60,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> MISHU</span>
+        <span> MISHU AI</span>
       </a>
     </span>
   );
